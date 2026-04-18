1f33a18c7b0ec6149861360d527febf1ef324172
Beloved1310 <adeoluwafisayomi@gmail.com>
2026-04-18 18:42:26 +0100

    feat: add error handling

--- project1/main.py
+++ project1/main.py
@@ -5,7 +5,7 @@ while True:
     user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.strip()
 
-    if "add" in user_action:
+    if user_action.startswith("add"):
         todo = user_action[4:]
         with open("project1/todos.txt", "r") as file:
             todos = file.readlines()
@@ -15,7 +15,7 @@ while True:
         with open("project1/todos.txt", "w") as file:
             todos = file.writelines(todos)
 
-    elif "show" in user_action:
+    elif user_action.startswith("show"):
         with open("project1/todos.txt", "r") as file:
             todos = file.readlines()
 
@@ -24,28 +24,39 @@ while True:
             row = f"{index + 1}-{item}"
             # item = item.title()
             print(row)
-    elif "edit" in user_action:
-        number = int(user_action[5:])
-        number = number - 1
-        with open("project1/todos.txt", "r") as file:
-            todos = file.readlines()
-        print("Here is todos existing", todos)
-        new_todo = input("Enter new todo: ")
-        todos[number] = new_todo + "\n"
+    elif user_action.startswith("edit"):
+        try:
+            number = int(user_action[5:])
+            number = number - 1
+            with open("project1/todos.txt", "r") as file:
+                todos = file.readlines()
+            print("Here is todos existing", todos)
+            new_todo = input("Enter new todo: ")
+            todos[number] = new_todo + "\n"
 
-        with open("project1/todos.txt", "w") as file:
-            todos = file.writelines(todos)
-    elif "complete" in user_action:
-        number = int(user_action[9:])
-        todos = file.readlines()
-        index = number - 1
-        todo_to_remove = todos[index].strip("\n")
-        todos.pop(index)
-        with open("project1/todos.txt", "w") as file:
-            file.writelines(todos)
-        message = f"Todo {todo_to_remove} was removed from the list."
-        print(message)
-    elif "exit" in user_action:
+            with open("project1/todos.txt", "w") as file:
+                todos = file.writelines(todos)
+        except ValueError:
+            print("Your command is not valid.")
+            continue
+
+    elif user_action.startswith("complete"):
+        try:
+            number = int(user_action[9:])
+            with open("project1/todos.txt", "r") as file:
+                todos = file.readlines()
+            
+            index = number - 1
+            todo_to_remove = todos[index].strip("\n")
+            todos.pop(index)
+            with open("project1/todos.txt", "w") as file:
+                file.writelines(todos)
+            message = f"Todo {todo_to_remove} was removed from the list."
+            print(message)
+        except IndexError:
+            print("There is no item with that number.")
+            continue
+    elif user_action.startswith("exit"):
         break
     else:
         print("Command is not valid")
