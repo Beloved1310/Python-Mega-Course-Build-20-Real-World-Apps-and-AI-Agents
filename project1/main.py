3b5166e2441f35275576f1f362b904e90d0a6658
Beloved1310 <adeoluwafisayomi@gmail.com>
2026-04-25 14:28:57 +0100

    feat: function arguments

--- project1/main.py
+++ project1/main.py
@@ -1,25 +1,28 @@
 # todo
-def get_todos():
-    with open("project1/todos.txt", "r") as file_local:
+def get_todos(filepath):
+    with open(filepath, "r") as file_local:
         todos_local = file_local.readlines()  
     return todos_local   
 
+def write_todos(filepath, todos_arg):
+    with open(filepath, "w") as file:
+        todos = file.writelines(todos_arg)
+        
 while True:
-    # Get userr input and strip space chars from it
+    # Get user input and strip space chars from it
     user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.strip()
 
     if user_action.startswith("add"):
         todo = user_action[4:]
-        todos = get_todos()
+        todos = get_todos("project1/todos.txt")
 
         todos.append(todo + "\n")
 
-        with open("project1/todos.txt", "w") as file:
-            todos = file.writelines(todos)
+        write_todos("project1/todos.txt", todos)
 
     elif user_action.startswith("show"):
-        todos = get_todos()
+        todos = get_todos("project1/todos.txt")
 
         for index, item in enumerate(todos):
             item = item.strip("\n")
@@ -30,14 +33,12 @@ while True:
         try:
             number = int(user_action[5:])
             number = number - 1
-            with open("project1/todos.txt", "r") as file:
-                todos = file.readlines()
+            todos = get_todos("project1/todos.txt")
             print("Here is todos existing", todos)
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo + "\n"
 
-            with open("project1/todos.txt", "w") as file:
-                todos = file.writelines(todos)
+            write_todos("project1/todos.txt", todos)
         except ValueError:
             print("Your command is not valid.")
             continue
@@ -45,13 +46,12 @@ while True:
     elif user_action.startswith("complete"):
         try:
             number = int(user_action[9:])
-            todos = get_todos()
+            todos = get_todos("project1/todos.txt")
             
             index = number - 1
             todo_to_remove = todos[index].strip("\n")
             todos.pop(index)
-            with open("project1/todos.txt", "w") as file:
-                file.writelines(todos)
+            write_todos("project1/todos.txt", todos) 
             message = f"Todo {todo_to_remove} was removed from the list."
             print(message)
         except IndexError:
